79499a8f30f7c8bf5f43e0e7550a76fbddeef2f5
Jonah Spear <Jonahspear512@gmail.com>
2018-02-07 13:23:07 +0700

    Add jquery to project

--- app/assets/javascripts/show_question.js
+++
@@ -1,9 +0,0 @@
- $(document).ready(function() {
-
- $('.questioncontainer').hide(); //Initially form wil be hidden.
-
-  $('#button_id').click(function() {
-   $('#form_id').show();//Form shows on button click
-
-   });
- });
