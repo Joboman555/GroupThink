2fb3f0f4a6404eb514fc12fdcd4a1e22c9ad26d3
Jonah Spear <Jonahspear512@gmail.com>
2018-02-12 12:08:45 +0700

    test if question submission creates a new question

--- test/controllers/docs_controller_test.rb
+++ test/controllers/docs_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class DocsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get docs_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get docs_index_url
+  #   assert_response :success
+  # end
 
 end
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "submitting form should create new model" do
+    assert_nil Question.find_by(title: "This is a test question")
+    post "/questions", params: { question: { title: "This is a test question", text: "Lorem ipsum upsum" } }
+    assert_not_nil Question.find_by(title: "This is a test question")
+  end
 end
