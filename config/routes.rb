952da0c8859c7ef59cb6e83875949d5e6d524dc9
Jonah Spear <Jonahspear512@gmail.com>
2018-02-25 18:51:25 +0700

    add initial draft of landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   get 'docs/index'
   get 'questions/hide', :to => 'questions#hide'
+  get 'welcome/index'
 
   resources :comments
   resources :questions
 
-  root 'docs#index'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
